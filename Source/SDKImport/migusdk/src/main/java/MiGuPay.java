6c54f0ce1444a00ce16a3a31e2fca3c27943ef31
GaoKai <kxjhaha@vip.qq.com>
2018-04-11 18:41:49 +0800

    提交

---
+++ Source/SDKImport/migusdk/src/main/java/MiGuPay.java
@@ -0,0 +1,18 @@
+import org.json.JSONObject;
+
+import SdkInterface.PayBase;
+
+/**
+ * Created by GaiKai on 2018/4/10.
+ */
+
+public class MiGuPay extends PayBase
+{
+    @Override
+    public void Init(JSONObject json)
+    {
+        super.Init(json);
+    }
+
+
+}
--- Source/SDKImport/sdkinterface/src/main/java/SdkInterface/LogBase.java
+++ Source/SDKImport/sdkinterface/src/main/java/SdkInterface/LogBase.java
@@ -16,14 +16,14 @@ public class LogBase extends SDKBase
             String logFunction = json.getString(SDKInterfaceDefine.Log_FunctionName);
 
             switch (logFunction) {
-                case SDKInterfaceDefine.Log_FunctionName_Login:
-                    Login(json);
-                    break;
-                case SDKInterfaceDefine.Log_FunctionName_LoginOut:
-                    LoginOut(json);
-                    break;
-                case SDKInterfaceDefine.Log_FunctionName_Event:
-                    OnEvent(json);
+                    case SDKInterfaceDefine.Log_FunctionName_Login:
+                        Login(json);
+                        break;
+                    case SDKInterfaceDefine.Log_FunctionName_LoginOut:
+                        LoginOut(json);
+                        break;
+                    case SDKInterfaceDefine.Log_FunctionName_Event:
+                        OnEvent(json);
                     break;
                 default:
                     SendError("Dont support Log_FunctionName " + logFunction, null);
